75100ec926ab73685386c5e24da8dcbc16e758ea
steve withington <steve@digitalmine.com>
2023-04-07 17:42:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,16 +56,16 @@ except URLError as e:
   streamlit.error()
 
 # Snowflake Connector
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('View Our Fruit List - Add Your Favorites!'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
-
-#streamlit.stop()
   
 # allow end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   res = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(res)
