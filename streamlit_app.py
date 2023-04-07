751c49a430cd70a7e35809e79fbbf4374f1e62ea
steve withington <steve@digitalmine.com>
2023-04-07 16:24:12 -0500

    Update streamlit_app.py
    normalize json fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# writes data to the screen
+# streamlit.text(fruityvice_response.json())
+
+# normalize the json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output table to screen
+streamlit.datafram(fruityvice_normalized)
